64a7a34464ba5d34aeabe43ddd6bb2f7c7ab0bd1
vudayani <vudayani@vmware.com>
2024-08-01 15:19:56 +0530

    Add visitErroneous to all java parser visitors

--- rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21ParserVisitor.java
+++ rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21ParserVisitor.java
@@ -197,6 +197,17 @@ public class ReloadableJava21ParserVisitor extends TreePathScanner<J, Space> {
     }
 
     @Override
+    public J visitErroneous(ErroneousTree node, Space fmt) {
+        System.out.println("Reloadable java 21");
+        String erroneousNode = source.substring(((JCTree) node).getStartPosition(), ((JCTree) node).getEndPosition(endPosTable));
+        return new J.Erroneous(
+                randomId(),
+                fmt,
+                Markers.EMPTY,
+                erroneousNode);
+    }
+
+    @Override
     public J visitBinary(BinaryTree node, Space fmt) {
         Expression left = convert(node.getLeftOperand());
 
@@ -1763,6 +1774,12 @@ public class ReloadableJava21ParserVisitor extends TreePathScanner<J, Space> {
             case RETURN:
             case THROW:
             case EXPRESSION_STATEMENT:
+                ExpressionTree expTree = ((ExpressionStatementTree) t).getExpression();
+                if (expTree instanceof ErroneousTree) {
+                    return Space.build(source.substring(((JCTree) expTree).getEndPosition(endPosTable),((JCTree) t).getEndPosition(endPosTable)), Collections.emptyList());
+                } else {
+                    return sourceBefore(";");
+                }
             case VARIABLE:
             case YIELD:
                 return sourceBefore(";");
